80cbe1f3cddb4e7d1dcebeb1ec91440bc025d2a1
Doug <clarke.doug248@gmail.com>
2015-05-11 20:54:55 -0400

    finish project, with error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,12 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    begin
+      user = User.find(params[:id])
+      user.destroy
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   
+  get 'list/new'
+
   get 'item/new'
 
   devise_for :users
 
   resources :users do
+    resources :lists
     resources :items, only: [:create, :destroy]
   end
 
@@ -16,6 +19,17 @@ Rails.application.routes.draw do
 
   get 'welcome/about' =>'welcome#about', as: :welcome_about
 
+  namespace :api, defaults: { format: :json } do
+    resources :users do
+       resources :lists
+     end
+     
+    resources :lists, only: [] do
+      resources :items, only: [:create]
+    end
+
+    resources :items, only: [:destroy]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
